f849acba9c6610c691ae3f0a1a99ba6b137cacef
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-02-26 12:33:42 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,53 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
-    extend: {},
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: '10',
+      11: '11',
+      12: '12',
+      13: '13',
+      14: '14',
+      15: '15',
+      16: '16',
+    },
+    extend: {
+      containers: {
+        '2xs': '16rem',
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    aspectRatio: ['responsive', 'hover']
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    plugin(function ({ addUtilities, addComponents, e, config, theme }) {
+
+    })
+  ],
 }
 
