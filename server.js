618f369beeea66c20fca0a800d19ab2c919613dd
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-04 15:46:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ app.engine("html", require("ejs").renderFile)
 app.get("/", function (req, res) {
     res.render("index.html")
 })
+
 app.get("/biography", function (req, res) {
     res.render("biography.html")
 })
+
 app.get("/blog", function (req, res) {
     res.render("blog.html")
 })
+
 app.get("/galery", function (req, res) {
     res.render("galery.html")
 })
 
 app.use(express.static('views'));
+
+app.get("*", function (req, res) {
+    res.render("404.html")
+})
 
 app.listen(80);
