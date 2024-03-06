d7a56733b4be4fef1f9b6178cff42bfdf903e7de
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-06 11:52:39 -0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-2JYZE1GNX7');
+
 $(document).ready(function (event) {
     $("#view-pop-close").click(function (event) {
         $("#view-pop").addClass("opacity-0")
@@ -6,43 +12,6 @@ $(document).ready(function (event) {
             $("#view-pop").removeClass("scale-[1.05]");
             $("#view-pop").addClass("hidden")
         }, 100);
-    })
-
-    $('button#login-user').click(function (e) {
-        PopContent({
-            element: `
-            <h1 class="font-semibold uppercase text-[40px] font-['Heavitas'] mt-[10px] mb-[20px] text-center">
-                        Login
-                    </h1>
-                    <div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
-                        <h1
-                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
-                            E-mail</h1>
-                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
-                            type="email" name="email" id="login-email">
-                    </div>
-                    <div class="bg-[#1A2431] rounded-md w-full mt-[15px] shadow-[#0F131A] shadow-xl">
-                        <h1
-                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
-                            Senha</h1>
-                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
-                            type="password" name="password" id="login-email">
-                    </div>
-                    <button
-                        class="bg-[#2B4C77] active:bg-[#2B4C77] hover:bg-[#3c6396] w-full p-2 mt-[15px] rounded-md transition text-[20px]"
-                        id="login-send">Entrar</button>
-                    <div class="mb-[10px] mt-[5px] flex justify-between">
-                        <button class="text-[#787878]" id="password-reset">Esqueci a minha senha</button>
-                        <button class="text-[#005C9F] group flex items-center gap-[5px]" id="register-option">Me
-                            registrar <i
-                                class="transition fa-solid fa-arrow-right group-hover:translate-x-1"></i></button>
-                    </div>
-            `,
-            classes: "flex flex-col justify-center p-5",
-            pop_display_delete: "w-[350px] h-[150px]",
-            pop_display_add: "w-[400px]"
-        })
-        visibilityOrHiddenPop();
     })
 
     $(document).click(function (event) {
@@ -132,4 +101,190 @@ function PopContent({ element = "Erro, elemento não encontrado...", classes = "
     $("#view-pop-content").html(element).removeClass().addClass(classes);
     if (pop_display_delete !== null) $("#view-pop-display").removeClass(`${pop_display_delete}`);
     if (pop_display_add !== null) $("#view-pop-display").addClass(`${pop_display_add}`);
+}
+
+$(document).ready(function () {
+    $('body').on('click', 'button#login-user', function (event) {
+        $("#view-pop").addClass("opacity-0")
+        setTimeout(() => {
+            $("#view-pop").removeClass("flex")
+            $("#view-pop").removeClass("scale-[1.05]");
+            $("#view-pop").addClass("hidden")
+        }, 100);
+        setTimeout(() => {
+            PopContent({
+                element: `
+            <h1 class="font-medium uppercase text-[40px] font-['Heavitas'] mt-[10px] mb-[20px] text-center">
+                        Login
+                    </h1>
+                    <form onsubmit="if (!window.__cfRLUnblockHandlers) return false; return false;">
+                    <div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            E-mail</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="email" name="email" id="login-email">
+                    </div>
+                    <div class="bg-[#1A2431] rounded-md w-full mt-[15px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            Senha</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="password" name="password" id="login-password">
+                    </div>
+                    <button
+                        class="bg-[#2B4C77] active:bg-[#2B4C77] hover:bg-[#3c6396] w-full p-2 mt-[15px] rounded-md transition text-[20px]"
+                        id="login-send" type="submit">Entrar</button>
+                        </form>
+                    <div class="mb-[10px] mt-[5px] flex justify-between">
+                        <button class="text-[#787878]" id="password-reset">Esqueci a minha senha</button>
+                        <button class="text-[#005C9F] group flex items-center gap-[5px]" id="register-option">Me
+                            registrar <i
+                                class="transition fa-solid fa-arrow-right group-hover:translate-x-1"></i></button>
+                    </div>
+            `,
+                classes: "flex flex-col justify-center p-5",
+                pop_display_delete: "w-[350px] h-[150px]",
+                pop_display_add: "w-[400px]"
+            })
+            visibilityOrHiddenPop();
+        }, 150)
+    })
+
+    $('body').on('click', 'button#register-option', function (event) {
+        $("#view-pop").addClass("opacity-0")
+        setTimeout(() => {
+            $("#view-pop").removeClass("flex")
+            $("#view-pop").removeClass("scale-[1.05]");
+            $("#view-pop").addClass("hidden")
+        }, 100);
+        setTimeout(() => {
+            PopContent({
+                element: `
+            <h1 class="font-medium uppercase text-[40px] font-['Heavitas'] mt-[10px] mb-[20px] text-center">
+                        Registrar
+                    </h1>
+                    <form onsubmit="if (!window.__cfRLUnblockHandlers) return false; return false;">
+					<div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            Nome</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="text" name="name" id="register-name">
+                    </div>
+                    <div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            E-mail</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="email" name="email" id="register-email">
+                    </div>
+                    <div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            Senha</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="password" name="password" id="register-password">
+                    </div>
+					<div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            Repetir Senha</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="password" name="password_verify" id="register-password-verify">
+                    </div>
+                    <button
+                        class="bg-[#2B4C77] active:bg-[#2B4C77] hover:bg-[#3c6396] w-full p-2 mt-[15px] rounded-md transition text-[20px]"
+                        id="register-send">Registrar Conta</button>
+                        </form>
+                    <div class="mb-[10px] mt-[5px] flex justify-between">
+                        <button class="text-[#005C9F] group flex items-center gap-[5px]" id="login-user">Já tenho conta<i
+                                class="transition fa-solid fa-arrow-right group-hover:translate-x-1"></i></button>
+                    </div>
+            `,
+                classes: "flex flex-col justify-center p-5",
+                pop_display_delete: "w-[350px] h-[150px]",
+                pop_display_add: "w-[400px]"
+            })
+            visibilityOrHiddenPop();
+        }, 150)
+    })
+
+    $('body').on('click', 'button#password-reset', function (event) {
+        $("#view-pop").addClass("opacity-0")
+        setTimeout(() => {
+            $("#view-pop").removeClass("flex")
+            $("#view-pop").removeClass("scale-[1.05]");
+            $("#view-pop").addClass("hidden")
+        }, 100);
+        setTimeout(() => {
+            PopContent({
+                element: `
+            <h1 class="font-medium uppercase text-[40px] font-['Heavitas'] mt-[10px] mb-[20px] text-center">
+                        Recuperação
+                    </h1>
+                    <form onsubmit="if (!window.__cfRLUnblockHandlers) return false; return false;">
+                    <div class="bg-[#1A2431] rounded-md w-full mt-[10px] shadow-[#0F131A] shadow-xl">
+                        <h1
+                            class="absolute mt-[-10px] ml-[15px] p-0 pr-3 pl-3 bg-[#1D4272] text-[#C2C2C2] text-[14px] rounded-full shadow-[#0F131A] shadow-lg">
+                            E-mail</h1>
+                        <input class="p-1 pl-3 pr-3 text-[18px] text-[#d7d7d7] bg-transparent pt-[16px] w-[100%]"
+                            type="email" name="email" id="login-email">
+                    </div>
+                    <button
+                        class="bg-[#2B4C77] active:bg-[#2B4C77] hover:bg-[#3c6396] w-full p-2 mt-[15px] rounded-md transition text-[20px]"
+                        id="code-reset-send">Enviar código</button>
+                        </form>
+					<div class="mb-[10px] mt-[5px] flex justify-between">
+                        <button class="text-[#005C9F] group flex items-center gap-[5px]" id="login-user">Lembrei a senha<i
+                                class="transition fa-solid fa-arrow-right group-hover:translate-x-1"></i></button>
+					</div>
+            `,
+                classes: "flex flex-col justify-center p-5",
+                pop_display_delete: "w-[350px] h-[150px]",
+                pop_display_add: "w-[400px]"
+            })
+            visibilityOrHiddenPop();
+        }, 150)
+    })
+
+    $('body').on('click', 'button#login-send', function (event) {
+        notifyView({ content: `<i class="fa-regular fa-circle-exclamation mb-[5px]"></i>Sistema de autenticação em construção, tente novamente mais tarde...` })
+    })
+    $('body').on('click', 'button#register-send', function (event) {
+        notifyView({ content: `<i class="fa-regular fa-circle-exclamation mb-[5px]"></i>Sistema de autenticação em construção, tente novamente mais tarde...` })
+    })
+    $('body').on('click', 'button#code-reset-send', function (event) {
+        notifyView({ content: `<i class="fa-regular fa-circle-exclamation mb-[5px]"></i>Sistema de autenticação em construção, tente novamente mais tarde...` })
+    })
+})
+
+function notifyView({ content, type = 0 }) {
+    var elementBase = $("#notification-view");
+    var dateId = Date.now();
+
+    var colors = [
+        "#00000075"
+    ]
+
+    var item = $("<div>")
+        .addClass(`p-0 pr-5 pl-5 rounded-full pointer-events-auto cursor-pointer`)
+        .css({ "background-color": colors[type] })
+        .attr("id", `notify-${dateId}`)
+        .html(`
+        <div class="p-2 pr-8 pl-8 rounded-full pointer-events-auto cursor-pointer">
+            <span class="flex gap-[15px] items-center justify-center">${content}</span>
+        </div>
+        `)
+
+    elementBase.append(item);
+
+    var deleteNotifyTime = setTimeout(() => {
+        $(`div#notify-${dateId}`).remove();
+    }, 5000)
+
+    $(`div#notify-${dateId}`).click(function (v) {
+        clearTimeout(deleteNotifyTime);
+        $(this).remove();
+    })
 }
