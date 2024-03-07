490266944d84b7e3ed08a8984f209568131b6b82
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-07 01:32:22 -0300

    Update router.js

---
+++ modules/pages/router.js
@@ -0,0 +1,32 @@
+module.exports.pagesRedirect = (db, pudding) => {
+    const express = require("express");
+    const app = express.Router();
+
+    app.get("/", function (req, res) {
+        res.render("index.html")
+    })
+
+    app.get("/biography", function (req, res) {
+        res.render("biography.html")
+    })
+
+    app.get("/blog", function (req, res) {
+        res.render("blog.html")
+    })
+
+    app.get("/galery", function (req, res) {
+        res.render("galery.html")
+    })
+
+    app.get("/textest", function (req, res) {
+        res.render("textecss.html")
+    })
+
+    app.use(express.static('views'));
+
+    app.get("*", function (req, res) {
+        res.render("404.html")
+    })
+
+    return app;
+}
