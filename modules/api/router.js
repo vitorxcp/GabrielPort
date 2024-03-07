67fb6430b9e84d08583df98d38fe14c3b27e3ad7
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-07 11:01:22 -0300

    Update router.js

--- modules/api/router.js
+++ modules/api/router.js
@@ -9,6 +9,7 @@
  * @typedef {Object} Pudding
  * @property {function} verifyEmailExist - Método para verificar se um e-mail existe no banco de dados
  * @property {function} registerAccount - Método para registrar uma nova conta no banco de dados
+ * @property {function} authenticateUser - Método para autenticar no sistema no banco de dados
  */
 
 /**
@@ -18,7 +19,7 @@
  * @returns {express.Router} O roteador de API express configurado
  */
 
-module.exports.apiRouter = (db, Pudding) => {
+module.exports.apiRouter = (db, Pudding, {passport}) => {
     const express = require("express");
     const app = express.Router();
 
@@ -26,14 +27,21 @@ module.exports.apiRouter = (db, Pudding) => {
 
     app.post('/auth/register', (req, res) => {
         const { name, email, password } = req.body;
+
         if (!name || !email || !password) {
-            return res.status(400).send('Todos os campos são obrigatórios');
+            return res.status(400).send({ message: 'Todos os campos são obrigatórios.' });
         }
+
         const existingUser = Pudding.verifyEmailExist(email);
+
         if (existingUser) {
-            return res.status(400).send('E-mail já registrado');
+            return res.status(400).send({ message: 'E-mail já registrado.' });
         }
-        Pudding.registerAccount(name, email, password)
+
+        var register = Pudding.registerAccount(name, email, password);
+
+        if (register === true) return res.status(200).send({ message: "Registrado com sucesso!" });
+        else res.status(500).send({ message: "Ocorreu algum imprevisto na hora de registrar, tente novamente mais tarde!" });
     });
 
     return app;
