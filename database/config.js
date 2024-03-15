5fff0ff07b12fd5abab6da79e21b7477fed9d943
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-15 03:00:38 -0300

    Update config.js
    Adicionamos sistemas mais complexos para registrar e verificar a autenticação do usuário indenpente se existe ou não.

--- database/config.js
+++ database/config.js
@@ -3,7 +3,8 @@
  * @property {function} verifyEmailExist - Método para verificar se um e-mail existe no banco de dados
  * @property {function} registerAccount - Método para registrar uma nova conta no banco de dados
  * @property {function} authenticateUser - Método para autenticar no sistema no banco de dados
- */
+ * @property {function} getAccount - Método para pegar os dados da conta no banco de dados
+*/
 
 /**
  * @typedef {Object} Database
@@ -16,15 +17,13 @@
  * @returns {Promise<Database>} Uma promessa que resolve com o banco de dados e a classe pudding
  */
 
+const { initializeApp } = require('firebase/app');
+const { getDatabase, ref, set, child, get } = require('firebase/database');
 const bcrypt = require('bcrypt');
 const ConfigProject = require('../config');
 
 module.exports.configDatabase = () => {
     return new Promise((resolve, reject) => {
-        // Importar as funções necessárias dos SDKs
-        const { initializeApp } = require("firebase/app");
-        const { getDatabase } = require("firebase/database"); // Importar o módulo do banco de dados
-
         const firebaseConfig = {
             apiKey: ConfigProject.database.apiKey,
             authDomain: ConfigProject.database.authDomain,
@@ -36,17 +35,24 @@ module.exports.configDatabase = () => {
             measurementId: ConfigProject.database.measurementId
         };
 
-        // Inicializar o Firebase
+        // Inicialize o Firebase
         const app = initializeApp(firebaseConfig);
-
-        const db = getDatabase(); // Inicializar o banco de dados
+        const db = getDatabase(app);
 
         class Pudding {
             static verifyEmailExist = async (email) => {
                 email = String(email).replaceAll(".", "_");
 
-                const data = await db.ref(`auth/accounts/${email}`).once("value");
-                return data.val() ? true : false;
+                const data = await get(child(ref(db), `auth/accounts/${email}`));
+                console.log(data.exists(), data.val())
+                return data.exists();
+            }
+
+            static getAccount = async (email) => {
+                email = String(email).replaceAll(".", "_");
+
+                const data = await get(child(ref(db), `auth/accounts/${email}`));
+                return data.val();
             }
 
             static registerAccount = async (name, email, password) => {
@@ -54,10 +60,12 @@ module.exports.configDatabase = () => {
                     email = String(email).replaceAll(".", "_");
 
                     const hashedPassword = await bcrypt.hash(password, 10);
-                    await db.ref(`auth/accounts/${email}`).set({
+                    await set(ref(db, `auth/accounts/${email}`), {
                         name,
+                        email,
                         password: hashedPassword
                     });
+                    console.log("e")
                     return true;
                 } catch (error) {
                     console.error(error);
@@ -69,20 +77,18 @@ module.exports.configDatabase = () => {
                 try {
                     email = String(email).replaceAll(".", "_");
 
-                    // Recupere a senha criptografada do banco de dados para o usuário com base no e-mail fornecido
-                    const data = await db.ref(`auth/accounts/${email}`).once('value');
+                    const data = await get(child(ref(db), `auth/accounts/${email}`));
                     const userData = data.val();
 
-                    // Verifique se o usuário existe e se a senha está correta
                     if (userData && await bcrypt.compare(password, userData.password)) return true;
                     else return false;
                 } catch (error) {
                     console.error('Erro ao autenticar usuário:', error);
-                    return false; // Autenticação falhou
+                    return false;
                 }
             };
         }
 
-        resolve({ db, Pudding });
+        resolve({ db, Pudding, credentialAdmin: ConfigProject.database.admin });
     });
 }
