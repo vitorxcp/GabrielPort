afd1ebc0e6807f9a440cd48790c07572bdc1ba55
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2024-03-15 23:09:28 -0300

    Update router.js

--- modules/pages/router.js
+++ modules/pages/router.js
@@ -1,9 +1,31 @@
-module.exports.pagesRedirect = (db, pudding) => {
+/**
+ * @typedef {Object} Database
+ * @property {any} db - Objeto do banco de dados Firebase
+ */
+
+/**
+ * @typedef {Object} Pudding
+ * @property {function} verifyEmailExist - Método para verificar se um e-mail existe no banco de dados
+ * @property {function} registerAccount - Método para registrar uma nova conta no banco de dados
+ * @property {function} authenticateUser - Método para autenticar no sistema no banco de dados
+ * @property {function} getAccount - Método para pegar os dados da conta no banco de dados
+ * @property {function} getRecoverPasswordCode - Método gerar e salvar o codigo de recuperação
+ * @property {function} recoverPasswordCode - Método para verificar o codigo de verificação é remover do banco de dados
+ * @property {function} passwordUpdate - Método para mudar senha do usuário
+ */
+
+/**
+ * Cria e retorna um roteador de API express
+ * @param {Database} db - Objeto do banco de dados
+ * @param {Pudding} Pudding - Objeto da classe pudding
+ * @returns {express.Router} O roteador de API express configurado
+ */
+
+module.exports.pagesRedirect = (db, Pudding) => {
     const express = require("express");
     const app = express.Router();
 
     app.get("/", function (req, res) {
-        console.log(req.user)
         res.render("index.html")
     })
 
@@ -21,6 +43,11 @@ module.exports.pagesRedirect = (db, pudding) => {
 
     app.get("/textest", function (req, res) {
         res.render("textecss.html")
+    })
+
+    app.get("/admin/painel", async (req, res) => {
+        if (!(req.isAuthenticated() ? (req.user.permissions ? req.user.permissions["admin"]: null) : null)) return res.redirect("/");
+        res.render("admin/painel.html")
     })
 
     app.use(express.static('views'));
